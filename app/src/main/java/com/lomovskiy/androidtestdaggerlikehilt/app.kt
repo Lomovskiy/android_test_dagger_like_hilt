d1dd2eea6784a9f61787d7e68821624dfe316a4a
Alexander Lomovskiy <a@lomovskiy.com>
2021-08-20 16:34:03 +0300

    Added AppComponent and injection into Application

--- app/src/main/java/com/lomovskiy/androidtestdaggerlikehilt/app.kt
+++ app/src/main/java/com/lomovskiy/androidtestdaggerlikehilt/app.kt
@@ -1,9 +1,60 @@
 package com.lomovskiy.androidtestdaggerlikehilt
 
 import android.app.Application
+import dagger.Binds
+import dagger.Component
+import dagger.Module
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class HiltApp : Application() {
+open class HiltApp : Application() {
 
-    
+    private val appComponent: AppComponent = DaggerAppComponent.create()
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent.inject(this as App)
+    }
 
 }
+
+class App : HiltApp() {
+
+    @Inject
+    lateinit var interactor: Interactor
+
+    override fun onCreate() {
+        super.onCreate()
+        print("")
+    }
+
+}
+
+@Component(modules = [AppModule::class])
+@Singleton
+interface AppComponent {
+
+    fun inject(app: App)
+
+}
+
+@Module
+interface AppModule {
+
+    @Binds
+    @Singleton
+    fun bindRepo(impl: RepoImpl): Repo
+
+    @Binds
+    @Singleton
+    fun bindInteractor(impl: InteractorImpl): Interactor
+
+}
+
+interface Repo
+class RepoImpl @Inject constructor() : Repo
+
+interface Interactor
+class InteractorImpl @Inject constructor(
+    private val repo: Repo
+) : Interactor
